519b45330b043267be2f1af05334f01e03ead1eb
Luz Valdovinos <luz.valdovinos@gmail.com>
2022-02-16 06:45:23 -0600

    added controller, model. deleted the scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Faker::Restaurant.type)
+  restaurant.save
+end
+
+puts 'Created 10 restaurants'
